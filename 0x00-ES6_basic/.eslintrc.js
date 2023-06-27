8adb04bcb65a714cef22ea8411a269ba59c7f27c
nothing4now <yusufkolu12345@gmail.com>
2023-06-27 15:55:57 +0100

    correcting

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    extends: ["airbnb-base", "plugin:jest/all"],
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    plugins: ["jest"],
+    rules: {
+      "no-console": "off",
+      "no-shadow": "off",
+      "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    },
+    overrides: [
+      {
+        files: ["*.js"],
+        excludedFiles: "babel.config.js",
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  };
